131696d3a512bf39016ae79f11dfc6ea07d47b9b
Claudiu Popa <pcmanticore@gmail.com>
2018-06-28 17:05:47 -0700

    Switch to using .post instead of .zapier for poetry compat

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='pynliner',
-      version='0.5.1.1.zapier',
+      version='0.5.1.1.post1',
       description='Python CSS-to-inline-styles conversion tool for HTML using'
                   ' BeautifulSoup and cssutils',
       author='Tanner Netterville',
