9b63f1de99eb752957055ed9b521158df21b6816
Claudiu Popa <pcmanticore@gmail.com>
2018-09-24 14:41:08 +0200

    Sync the version from __init__ with the one from setup.py and bump the version

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
+
 from setuptools import setup
 
+with open('pynliner/__init__.py') as stream:
+    content = stream.read()
+    version_match = re.search("__version__\s*=\s*'(.*?)'\s+", content)
+
 setup(name='pynliner',
-      version='0.5.1.1.post2',
+      version=version_match.group(1),
       description='Python CSS-to-inline-styles conversion tool for HTML using'
                   ' BeautifulSoup and cssutils',
       author='Tanner Netterville',
